ea7d0ca5e6357182eedfffde7a52ad7607d40d72
StevenDRiggs <sfriggs21@gmail.com>
2020-07-05 15:36:10 -0600

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,16 +1,11 @@
 module SongsHelper
   def artist_select(song)
     if !song.artist.nil?
-      <<-HTML
-        #{song.artist.name}
-        #{hidden_field_tag 'song_artist_id', song.artist_id}
-      HTML
+      song.artist.name
+      hidden_field_tag 'song_artist_id', song.artist_id
     else
-      <<-HTML
-        #{label_tag 'Artists', 'song_artist_id'}
-        <br>
-        #{select_tag 'song_artist_id', Artist.all, options_from_collection_for_select: [:id, :name]}
-      HTML
+      label_tag 'Artists', 'song_artist_id'
+      select_tag 'song_artist_id', Artist.all, options_from_collection_for_select: [:id, :name]
     end
   end
 
